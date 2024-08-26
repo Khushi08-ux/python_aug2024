1eb4292d6a21a5b21b6b8a2b32dfb224a1eaab27
Khushi08-ux <kallannavarkhushi@gmail.com>
2024-08-26 16:20:34 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,10 @@
+def my_function (some_list):
+    some_list.remove(10)
+    some_list.insert(1,25)
+    some_list.append(50)
+
+print("Enter space separated numbers")
+list1=list(map(int,input().split()))
+print(list1)
+my_function(list1) #call by refernce
+print(list1)
